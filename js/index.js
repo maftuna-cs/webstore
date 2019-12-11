afdd60ed66ee5d254ea8d641c33d814dcbae6c91
mac <mac@macui-MacBookPro.local>
2019-12-11 10:43:48 +0900

    .added-filtering

--- js/index.js
+++ js/index.js
@@ -13,10 +13,11 @@ const shoppingCart = [
 const allProducts = [
   { // 0
     id: 101, 
-    name: `Monogram Motif Leather Ankle Boots`,
-    img: `img/img-02.jpg`,
-    color: [`Black, Tan`],
-    size: [`35,36,37,38,39`],
+    name: `A Chevron Detail Point Toe Flat Loafers`,
+    img: `https://img.ltwebstatic.com/images3_pi/2019/11/07/15731206694c5f1df63ea8ba8d344656a9b372ff7d_thumbnail_900x.webp`,
+    price: `45`,
+    color: [`Khaki, Beige`],
+    size: [35,36,37,38,39],
     qty: true,
     rating: 5,
     category: null,
@@ -24,50 +25,197 @@ const allProducts = [
     id: 102, 
     name: `Suede Chunky Heeled Ankle Boots`,
     img: `img/img-02.jpg`,
+    price: `90`,
     color: [`Beige, Black`],
-    size: [`35,36,37,38,39`],
+    size: [35,36,37,38,39],
     qty: true,
     rating: 4,
     category: `development`,
   },{ // 2
     id: 103, 
-    name: `Suede Chunky Heeled Ankle Boots`,
+    name: `Strap Ballet Flats`,
     img: `img/img-02.jpg`,
+    price: `90`,
     color: [`Beige, Black`],
-    size: [`35,36,37,38,39`],
+    size: [35,36,37,38,39],
+    qty: false,
+    rating: 4,
+    category: `Flats`,
+  },{ // 3
+    id: 104, 
+    name: `Point Toe Embossed Stiletto Heels`,
+    img: `img/img-02.jpg`,
+    price: `90`,
+    color: [`Beige, Black`],
+    size: [35,36,37,38,39],
+    qty: true,
+    rating: 4,
+    category: `flats`,
+    available: 0
+  },{ // 4
+    id: 105, 
+    name: `Metal Decor Flat Mules`,
+    img: `https://img.ltwebstatic.com/images2_pi/2019/04/12/15550561893993284564_thumbnail_900x1199.webp`,
+    price: `90`,
+    color: [`Beige, Black`],
+    size: [35,36,37,38,39],
+    qty: true,
+    rating: 4,
+    category: `development`,
+  },{
+    id: 106, 
+    name: `Flat Loafers`,
+    img: `https://img.ltwebstatic.com/images2_pi/2019/07/15/1563187969227323736_thumbnail_900x1199.webp`,
+    price: `90`,
+    color: [`Black, Tan`],
+    size: [35,36,37,38,39],
+    qty: true,
+    rating: 5,
+    category: null,
+  },{
+    id: 107, 
+    name: `Lmond Toe Side Goring Block Heel Ankle Boots`,
+    img: `https://img.ltwebstatic.com/images2_pi/2019/08/01/15646517064065547572_thumbnail_900x1199.webp`,
+    price: `90`,
+    color: [`Black, Tan`],
+    size: [35,36,37,38,39],
+    qty: true,
+    rating: 5,
+    category: null,
+  },{
+    id: 108, 
+    name: `Rhinestone & Pearl Decor Plaid Flats`,
+    img: `https://img.ltwebstatic.com/images3_pi/2019/11/01/15725879729bd519b5c80256013d7e575f61fabe46_thumbnail_900x.webp`,
+    price: `90`,
+    color: [`Black, Tan`],
+    size: [35,36,37,38,39],
+    qty: true,
+    rating: 5,
+    category: null,
+  },{
+    id: 106, 
+    name: `Studded Decor Point Toe Flats`,
+    img: `https://img.ltwebstatic.com/images2_pi/2019/04/17/15554883673784057751_thumbnail_900x1199.webp`,
+    price: `90`,
+    color: [`Black, Tan`],
+    size: [35,36,37,38,39],
+    qty: true,
+    rating: 5,
+    category: null,
+  },{
+    id: 106, 
+    name: `Faux Pearl Decor Buckle Strap Flats`,
+    img: `https://img.ltwebstatic.com/images3_pi/2019/11/07/1573121904bbf3ee061cae0cc094c7b3284f1c1cd8_thumbnail_900x.webp`,
+    price: `90`,
+    color: [`Black, Tan`],
+    size: [35,36,37,38,39],
+    qty: true,
+    rating: 5,
+    category: null,
+  },
+  { // 0
+    id: 101, 
+    name: `Monogram Motif Leather Ankle Boots`,
+    img: `img/img-02.jpg`,
+    price: `90`,
+    color: [`Black, Tan`],
+    size: [35,36,37,38,39],
+    qty: true,
+    rating: 5,
+    category: null,
+  },{  // 1
+    id: 102, 
+    name: `Letter Graphic Lace-Up Front Chunky Sneakers`,
+    img: `https://img.ltwebstatic.com/images3_pi/2019/11/08/15732185642ac0600aeb7b018542944946a30ac1d7_thumbnail_900x.webp`,
+    price: `90`,
+    color: [`Beige, Black`],
+    size: [35,36,37,38,39],
+    qty: true,
+    rating: 4,
+    category: `development`,
+  },{ // 2
+    id: 103, 
+    name: `Toddler Girls Jewelled Decor Glitter Flats`,
+    img: `https://img.ltwebstatic.com/images3_pi/2019/11/09/15732975501a3b61309626809b8b25041ca3e6ed22_thumbnail_900x.webp`,
+    price: `90`,
+    color: [`Beige, Black`],
+    size: [35,36,37,38,39],
     qty: false,
     rating: 4,
     category: `design`,
   },{ // 3
     id: 104, 
-    name: `Point Toe Embossed Stiletto Heels`,
-    img: `img/img-02.jpg`,
-    color: [`Beige, Black`],
-    size: [`35,36,37,38,39`],
+    name: `Baby Girl Deer Design Faux Fur Lined Knit Boots`,
+    img: `https://img.ltwebstatic.com/images3_pi/2019/11/06/157303094067574d17d46f92044b2b18427b2f15cf_thumbnail_900x.webp`,
+    price: `90`,
+    color: [`One Color`],
+    size: [19,20,21],
     qty: true,
     rating: 4,
     category: `design`,
     available: 0
   },{ // 4
     id: 105, 
-    name: `Metal Decor Flat Mules`,
-    img: `img/img-02.jpg`,
+    name: `Baby Girl Bow Decor Flats`,
+    img: `https://img.ltwebstatic.com/images3_pi/2019/10/28/15722574028efca64a36ecf197b3d0f3173fea02f6_thumbnail_900x.webp`,
+    price: `90`,
     color: [`Beige, Black`],
-    size: [`35,36,37,38,39`],
+    size: [19,20,21],
     qty: true,
     rating: 4,
     category: `development`,
   },{
-    id: 101, 
+    id: 106, 
+    name: `Rhinestone & Bow Decor Glitter Flats`,
+    img: `https://img.ltwebstatic.com/images3_pi/2019/11/21/157432133935dbe629ced96fbbf59575cdd26068f2_thumbnail_900x.webp`,
+    price: `90`,
+    color: [`Black, Tan`],
+    size: [19,20,21],
+    qty: true,
+    rating: 5,
+    category: 'flats',
+  },{
+    id: 107, 
+    name: `Cartoon Embroidered Fluffy Flats`,
+    img: `https://img.ltwebstatic.com/images3_pi/2019/09/25/1569400241a0fc38976f68ac85d48c9929b84a290f_thumbnail_900x.webp`,
+    price: `90`,
+    color: [`Black, Tan`],
+    size: [35,36,37,38,39],
+    qty: true,
+    rating: 5,
+    category: null,
+  },{
+    id: 108, 
+    name: `Double Strap Flat Loafers`,
+    img: `https://img.ltwebstatic.com/images3_pi/2019/10/12/1570870578d104b7091eed877d2009361e4ff3ac90_thumbnail_900x.webp`,
+    price: `90`,
+    color: [`Black, Tan`],
+    size: [35,36,37,38,39],
+    qty: true,
+    rating: 5,
+    category: null,
+  },{
+    id: 106, 
+    name: `Decor Point Toe Slingback Flats`,
+    img: `https://img.ltwebstatic.com/images3_pi/2019/11/26/15747622365eb869ac4121c6b94765a1699f4e777f_thumbnail_900x.webp`,
+    price: `90`,
+    color: [`Black, Tan`],
+    size: [35,36,37,38,39],
+    qty: true,
+    rating: 5,
+    category: null,
+  },{
+    id: 106, 
     name: `Flat Loafers`,
     img: `img/img-02.jpg`,
+    price: `90`,
     color: [`Black, Tan`],
-    size: [`35,36,37,38,39`],
+    size: [35,36,37,38,39],
     qty: true,
     rating: 5,
     category: null,
   }
-  
+ 
 ];
 
 
@@ -102,12 +250,12 @@ const loadProductsByOrder = (arrToSort, criteria) => {
 
   const sortedProducts = arrToSort.slice(); // clone the array
 
-  if (criteria == 'weeksAsc') {
+  if (criteria == 'priceAsc') {
     // Smallest to largest
-    sortedProducts.sort((a, b) => a.weeks - b.weeks);
-  } else if (criteria == 'weeksDesc') {
+    sortedProducts.sort((a, b) => a.price - b.price);
+  } else if (criteria == 'priceDesc') {
     // Largest to smallest
-    sortedProducts.sort((a, b) => b.weeks - a.weeks);
+    sortedProducts.sort((a, b) => b.price - a.price);
   } else if (criteria == 'nameAsc') {
     // Largest to smallest
     sortedProducts.sort((a, b) => a.name.localeCompare(b.name));
@@ -124,7 +272,8 @@ const runTheFilter = theForm => {
 
   const categoryToSearch = theForm.elements.categoryName.value;
   const nameToSearch = theForm.elements.productName.value;
-  const semesterToSearch = theForm.elements.semester.value;
+  const genderToSearch = theForm.elements.gender.value;
+  // const semesterToSearch = theForm.elements.semester.value;
 
   console.log(theForm.elements)
 
@@ -132,11 +281,82 @@ const runTheFilter = theForm => {
   const filteredproducts = allProducts
     .filter(c => c.name.toLowerCase().includes( nameToSearch.trim().toLowerCase() ))  // filter by name
     .filter(c => c.category == categoryToSearch || categoryToSearch == "all")         // filter by category
-    .filter(c => `${c.start.term } ${c.start.year}` == semesterToSearch || semesterToSearch == "all");        // filter by semester
-
+    .filter(productsByGender, genderToSearch)         // filter by category
+    // .filter(c => `${c.start.term } ${c.start.year}` == semesterToSearch || semesterToSearch == "all");        // filter by semester
+    productsByGender
   renderProductsFromArray(filteredproducts);
 }
 
+
+function productsByGender(prod) {
+
+  // let sizes;
+  const sizes = {
+    women: [35,36,37,38,39],
+    men: [40,41,42,43,44],
+    kids: [19,20,21]
+  }
+
+  let ret = false;
+  prod.size.forEach(sz => {
+    if (sizes[this].includes(sz)) {
+      ret = true;
+      return;
+    }
+  })
+  
+  return ret;
+}
+
+
+
+/* const productsByGender = () => {
+  // debugger;
+  console.log('clicked')
+  let sizes;
+
+  // if (document.getElementById("womenSize").checked) {
+  //   size = allProducts.filter(allProducts.size == [35,36,37,38,39].includes);
+  // } else if (document.getElementById("menSize").checked) {
+  //   size = allProducts.filter(allProducts.size == '[`40,41,42,43,44`]');
+  // } else if = (document.getElementById("kidsSize").checked) {
+  //   size = allProducts.filter(allProducts.size == '[`19,20,21`]');
+  // } else {
+  //   return;
+  // }
+
+  if (document.getElementById("womenSize").checked) {
+    sizes = [35,36,37,38,39];
+  } else if (document.getElementById("menSize").checked) {
+    sizes = [40,41,42,43,44];
+  } else if (document.getElementById("kidsSize").checked) {
+    sizes = [19,20,21];
+  } else {
+    return;
+  }
+
+  renderProductsFromArray(
+    allProducts.filter((prod) => {
+      let ret = false;
+      prod.size.forEach(sz => {
+        if (sizes.includes(sz)) {
+          ret = true;
+          // break;
+        }
+      })
+      return ret;
+      // (prod.size.find(sizes)) ? [...acc, num]: acc, []).length > 0
+    })
+  )
+
+  
+  // console.log(size, document.getElementById("womenSize"))
+}
+ */
+// let ar1 = [35,36,37,38,39];
+// let ar2 = [4,36,38,6];
+
+// ar1.reduce((acc, num) => (ar2.includes(num)) ? [...acc, num]: acc, [])
 
 
 // EVENT HANDLER FUNCTIONS **************
@@ -205,9 +425,11 @@ const getProductAsHtmlString = product => {
 
   return `
     <article class="product ${(product.category) ? `cat-${product.category}` : ''} ${soldout}">
-      <h3 id="name">${product.name} ${callout}</h3>
-      <img src="${product.img}" alt="${product.name}" class="product-image">
+    <img src="${product.img}" alt="${product.name}" class="product-image">
+    <h3 id="name">${product.name} ${callout}</h3>
+      
       <ul class="product-info">
+      <li>Price: <strong>$${product.price}</strong></li>
         <li>Colour: <strong>${product.color}</strong></li>
         <li>Size: <strong>${product.size}</strong></li>
         
@@ -276,7 +498,8 @@ window.addEventListener('load', () => {
   document.getElementById('productName').addEventListener('input', submitTheFilterForm);
   document.getElementById('categoryName').addEventListener('change', submitTheFilterForm);
   document.querySelectorAll('[name="semester"]').forEach(radbtn => radbtn.addEventListener('change', submitTheFilterForm));
-
+  document.querySelectorAll('[name="gender"]').forEach(radbtn => radbtn.addEventListener('change', submitTheFilterForm));
+  
 });
 
 

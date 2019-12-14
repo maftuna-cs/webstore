db1786d8a7f95881d2699e0686e933d3a98d6ed6
mac <mac@macui-MacBookPro.local>
2019-12-14 18:22:13 +0900

    .media-queries-updated

--- js/index.js
+++ js/index.js
@@ -462,7 +462,7 @@ window.addEventListener('load', () => {
   
 });
 
-function accordionMenu() {
+function responsiveMenu() {
   let menu = document.getElementById("toggled");
 	let menuStyle = window.getComputedStyle(menu);
 	let display = menuStyle.getPropertyValue('display');
@@ -478,7 +478,7 @@ function accordionMenu() {
 
 // add event listener to menu button
 let toggle = document.getElementById("menu-toggle");
-toggle.addEventListener("click", accordionMenu, false);
+toggle.addEventListener("click", responsive, false);
 
 
 
